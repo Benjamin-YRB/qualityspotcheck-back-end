0b83538d5bb2a711a18fe9dead0843bb38f259db
YRB <2576219848@qq.com>
2021-01-08 14:17:09 +0800

    init a springboot project

---
+++ src/main/java/com/focustar/qualityspotcheck/QualityspotcheckApplication.java
@@ -0,0 +1,13 @@
+package com.focustar.qualityspotcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualityspotcheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QualityspotcheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/focustar/qualityspotcheck/QualityspotcheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.focustar.qualityspotcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualityspotcheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
